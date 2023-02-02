eb8a8033f2b2c5da38e866c444bf772c15008a11
abdulrebel <113014752+abdulrebel@users.noreply.github.com>
2023-02-02 15:32:09 +0530

    armstrong commit

--- fibo.java
+++ fibo.java
@@ -25,13 +25,51 @@ class fibo{
 
     }
 
+   static boolean isArm(int n)
+   {
+    int temp,digits=0,last=0,sum=0;
+    temp=n;
+    while(temp>0)
+    {
+        temp=temp/10;
+        digits++;
+    }
+    temp=n;
+    while(temp>0)
+    {
+
+        last=temp%10;
+        sum+=(Math.pow(last, digits));
+        temp=temp/10;
+    }
+    if(n==sum)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+   }
+
+
+
     public static void main(String args[])
     {
         int count=20;
         System.out.print(n1+" "+n2);
         printFibo(count-2);
 
+        checkPrimeNum(4);
+
+        if(isArm(153)){
+            System.out.println("it is a arm no");
+        }
+    
+       System.out.println();
     }
+
+   
 
 }
 
